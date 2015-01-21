0d51d941edc87f02334933716871837ea3743beb
Chungsub Kim <chungsub.kim@purpleworks.co.kr>
2015-01-21 12:35:07 +0900

    first commit

--- README.md
+++ README.md
@@ -4,15 +4,65 @@ An angular directive wrapper for drawer.
 
 ## Getting started
 
+(1) Get angular-drawer via [Bower](http://bower.io/)
+
+```sh
+$ bower install angular-drawer
+```
+or add bower.json
+```sh
+$ bower install angular-drawer --save
+```
+
+(2) add css & javascript link to html
+
+```html
+...
+<link rel="stylesheet" href="bower_components/angular-drawer/dist/styles/angular-drawer.min.css">
+...
+<script src="bower_components/angular-drawer/dist/angular-drawer.min.js"></script>
+...
+```
+
+(3) add `'angular-drawer'` to your main module's list of dependencies
+
+```javascript
+var myApp = angular.module('myApp', ['angular-drawer']);
+```
+
+(4) enjoy!
+
 ## Quick example
-
-### options
-
-### controller
 
 ### view
 
-## Links
+default - left drawer
+
+```
+<div drawer>
+  Left Drawer
+</div>
+```
+
+right drawer
+
+```
+<div drawer="right">
+  right Drawer
+</div>
+```
+
+open drawer
+
+```
+<a ng-click="openLeftDrawer();">
+  open
+</a>
+```
+
+## Customizing
+
+TODO: customizing guide
 
 ## Contributing
 
--- src/angular-drawer.js
+++ src/angular-drawer.js
@@ -7,9 +7,10 @@
   'use strict';
 
   angular.module('angular-drawer', [])
-    .provider('drawer', function () {
+    .provider('drawer', function() {
       var opts = {
       };
+
       return {
         setOption: function (newOpts) {
           angular.extend(opts, newOpts);
@@ -19,11 +20,107 @@
         }
       };
     })
-    .directive('drawer', ['drawer', function (drawerOption) {
+    .directive('drawer', ['$window', '$rootScope', function ($window, $rootScope) {
+      var isInit = false;
+
+      var bgEl, leftDrawerEl, rightDrawerEl;
+      var bodyEl = angular.element(document.body);
+      var DRAWER_MODE = {
+        ALL: 0,
+        LEFT: 1,
+        RIGHT: 2
+      };
+
+      function onResize() {
+        var height = angular.element($window)[0].innerHeight + 'px';
+        bgEl.css('height', height);
+        if(leftDrawerEl) {
+          leftDrawerEl.css('height', height);
+        }
+        if(rightDrawerEl) {
+          rightDrawerEl.css('height', height);
+        }
+      }
+
+      function init() {
+        bgEl = angular.element(document.createElement('div'));
+        bgEl.addClass('ng-drawer-bg');
+        bgEl.bind('click', function() { closeDrawer(DRAWER_MODE.ALL); });
+        angular.element(document.body).append(bgEl);
+
+        onResize();
+        angular.element($window).bind('resize', onResize);
+
+        $rootScope.openRightDrawer = function() {
+          openDrawer(DRAWER_MODE.RIGHT);
+        };
+
+        $rootScope.openLeftDrawer = function() {
+          openDrawer(DRAWER_MODE.LEFT);
+        };
+
+        $rootScope.openDrawer = function() {
+          openDrawer(DRAWER_MODE.ALL);
+        };
+
+        $rootScope.closeRightDrawer = function() {
+          closeDrawer(DRAWER_MODE.RIGHT);
+        };
+
+        $rootScope.closeLeftDrawer = function() {
+          closeDrawer(DRAWER_MODE.LEFT);
+        };
+
+        $rootScope.closeDrawer = function() {
+          closeDrawer(DRAWER_MODE.ALL);
+        };
+      }
+
+      function openDrawer(mode) {
+        bgEl.addClass('active');
+        bodyEl.addClass('ng-drawer-no-scroll');
+
+        if(leftDrawerEl && 
+          (mode == DRAWER_MODE.LEFT || mode == DRAWER_MODE.ALL)) {
+          leftDrawerEl.addClass('active');
+        }
+        if(rightDrawerEl && 
+          (mode == DRAWER_MODE.RIGHT || mode == DRAWER_MODE.ALL)) {
+          rightDrawerEl.addClass('active');
+        }
+      }
+
+      function closeDrawer(mode) {
+        bgEl.removeClass('active');
+        bodyEl.removeClass('ng-drawer-no-scroll');
+
+        if(leftDrawerEl && 
+          (mode == DRAWER_MODE.LEFT || mode == DRAWER_MODE.ALL)) {
+          leftDrawerEl.removeClass('active');
+        }
+        if(rightDrawerEl && 
+          (mode == DRAWER_MODE.RIGHT || mode == DRAWER_MODE.ALL)) {
+          rightDrawerEl.removeClass('active');
+        }
+      }
+
       return {
-        restrict: 'A',
+        restrict: 'AE',
         link: function (scope, element, attrs) {
-          
+          if(!isInit) {
+            init();
+          }
+
+          element.addClass('ng-drawer');
+          element.css('height', angular.element($window)[0].innerHeight + 'px');
+
+          if(attrs.drawer == 'right') {
+            element.addClass('ng-drawer-right');
+            rightDrawerEl = element;
+          } else {
+            element.addClass('ng-drawer-left');
+            leftDrawerEl = element;
+          }          
         }
       };
     }]);
